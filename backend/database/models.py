c56f2525c3c628e72b0cecb46276fad1c12b1d7b
michellestapp <mstapp@rocketmail.com>
2023-06-06 18:58:57 -0500

    model complete

--- backend/database/models.py
+++ backend/database/models.py
@@ -10,6 +10,7 @@ class User(db.Model):
     first_name = db.Column(db.String(255), nullable=False)
     last_name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    contact_number = db.Column(db.String(255))
 
     def hash_password(self):
         self.password = generate_password_hash(self.password).decode('utf8')
@@ -31,3 +32,24 @@ class Car(db.Model):
     user = db.relationship("User")
 
 # TODO: Add your models below, remember to add a new migration and upgrade database
+class Garage_Sale(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    start_time = db.Column(db.Time, nullable = False)
+    end_time = db.Column(db.Time, nullable = False)
+    street_address = db.Column(db.String(255), nullable = False)
+    city = db.Column(db.String(255), nullable = False)
+    state = db.Column(db.String(255), nullable = False)
+    zip = db.Column(db.Integer, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship("User")
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name_of_item = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255))
+    price = db.Column(db.Integer)
+    category = db.Column(db.String(255))
+    image = db.Column(db.String(255))
+    gs_id = db.Column(db.Integer, db.ForeignKey('garage_sale.id'))
+    garage_sale = db.relationship("Garage_Sale")
