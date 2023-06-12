c780c5a10a8ac62496bc248b0abf483e1300c8a8
michellestapp <mstapp@rocketmail.com>
2023-06-12 12:19:42 -0500

    Updated Homepage to show date, start time and end time of losted garagesales

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -10,30 +10,48 @@ const HomePage = () => {
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
   const [cars, setCars] = useState([]);
+  const[garageSales, setGarageSales] = useState([]);
+
+
 
   useEffect(() => {
-    const fetchCars = async () => {
+    const fetchGarageSales = async () => {
       try {
-        let response = await axios.get("http://127.0.0.1:5000/api/user_cars", {
+        let response = await axios.get("http://127.0.0.1:5000/api/garage_sales", {
           headers: {
             Authorization: "Bearer " + token,
           },
         });
-        setCars(response.data);
+        const updatedGarageSales = response.data.map((garage_sale) => {
+          const date = new Date(garage_sale.date);
+          // const startTime = new Date(garage_sale.start_time)
+          // console.log(startTime)
+
+          const formattedDate = date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+          // const formattedStartTime = startTime.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' });
+          return {...garage_sale,
+            formattedDate: formattedDate,
+            // formattedStartTime: formattedStartTime
+          };
+        });
+
+        setGarageSales(updatedGarageSales);
       } catch (error) {
-        console.log(error.response.data);
+
+
       }
     };
-    fetchCars();
+    fetchGarageSales();
   }, [token]);
   return (
     <div className="container">
       {console.log(user)}
       <h1>Home Page for {user.username}!</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
+      {garageSales &&
+        garageSales.map((garage_sale) => (
+          <p key={garage_sale.id}>
+            {garage_sale.formattedDate} {garage_sale.start_time}-{garage_sale.end_time}
+            {/* {car.year} {car.model} {car.make} */}
           </p>
         ))}
     </div>
