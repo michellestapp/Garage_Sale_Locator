d96148a5d4b7d17d0ffd304a9aad325615070e76
michellestapp <mstapp@rocketmail.com>
2023-08-04 16:36:02 -0500

    Added default center as users current location

--- frontend/src/components/AllGarageSaleMap/AllGarageSaleMap.jsx
+++ frontend/src/components/AllGarageSaleMap/AllGarageSaleMap.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 import './AllGarageSaleMap.css'
+import { useState,useEffect } from "react";
 
 const LocationMarker = ({ text }) =>  
  <div style={{ backgroundColor: 'red', color: 'white', padding: '5px', borderRadius: '50%', width: '40px', height: '40px' }}>
@@ -12,8 +13,21 @@ const AllGarageSaleMap = () => {
     lat: 40.712776,
     lng: -74.005974,
   };
+  const [userLocation, setUserLocation] = useState(null);
 
   const defaultZoom = 10;
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setUserLocation({ lat: latitude, lng: longitude });
+      },
+      (error) => {
+        console.error("Error getting user's location:", error);
+      }
+    );
+  }, []);
   
 
   const locations = [
@@ -24,11 +38,16 @@ const AllGarageSaleMap = () => {
 
   return (
     <div className="large-map">
+
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
-        defaultCenter={defaultCenter}
+        defaultCenter={userLocation}
         defaultZoom={defaultZoom}
       >
+      {userLocation && (
+        <LocationMarker lat={userLocation.lat} lng={userLocation.lng} text="You are here" />
+      )}
+
         {locations.map((location) => (
           <LocationMarker
             key={location.id}
